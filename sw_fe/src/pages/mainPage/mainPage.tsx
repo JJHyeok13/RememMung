1c76905e4df1081a1cbbc735e649f2e4a5cf8f7c
JJHyeok13 <jjinh99@naver.com>
2024-05-27 01:53:42 +0900

    feat: mainPage 수정

--- sw_fe/src/pages/mainPage/mainPage.tsx
+++ sw_fe/src/pages/mainPage/mainPage.tsx
@@ -2,9 +2,6 @@ import React from "react";
 
 import styled from "styled-components";
 
-import PetImageExample from "../../assets/mainPage/PetImageExample.svg";
-import ChattingInput from "../../components/mainPage/chattingInput/chattingInput";
-
 const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -14,28 +11,39 @@ const Container = styled.div`
 
 const WhiteBox = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   width: 1080px;
   height: 551px;
   border-radius: 12px;
   background-color: #f4f4f4;
 
-  margin: 0 auto;
-`;
+  // 글씨 관련 CSS
+  font-size: 18px;
+  color: #5d606b;
+  line-height: 180%;
+  letter-spacing: -0.02em;
 
-const PetImage = styled.img`
-  width: 1080px;
-  height: 551px;
-  border-radius: 12px;
+  padding: 32px 16px 32px 32px;
+  box-sizing: border-box;
+
+  margin: 0 auto;
 `;
 
 const MainPage: React.FC = () => {
   return (
     <Container>
       <WhiteBox>
-        <PetImage src={PetImageExample} />
+        <div>반가워요!</div>
+        <div>
+          리멤멍은 반려동물과의 이별을 하게된 사람들을 위해 과거의 추억 회상을
+          통해
+        </div>
+        <div>이별을 준비하고 도와주는 것을 목표로 제작된 서비스에요..</div>
+        <div>당신의 반려동물과 건강한 이별을 리멤멍이 도와줄게요!</div>
+        <br />
+        <br />
       </WhiteBox>
-      <ChattingInput />
     </Container>
   );
 };
