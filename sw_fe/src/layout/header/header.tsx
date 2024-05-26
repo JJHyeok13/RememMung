89b1039200ac0833f4421c6dc1aeab69b5f8e7f2
JJHyeok13 <jjinh99@naver.com>
2024-05-27 03:27:48 +0900

    feat: header 라우팅 변경

--- sw_fe/src/layout/header/header.tsx
+++ sw_fe/src/layout/header/header.tsx
@@ -3,11 +3,18 @@ import React from "react";
 import styles from "./styles";
 
 import LogoImage from "../../assets/header/Logo.svg";
+import { useNavigate } from "react-router-dom";
 
 const Header: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleMypage = () => {
+    navigate("/mypage");
+  };
+
   return (
     <styles.Container>
-      <styles.StyledLink to="/">
+      <styles.StyledLink to="/main">
         <styles.LeftContainer>
           <styles.LogoImage src={LogoImage} alt="Logo Image" />
           <div>리멤멍</div>
@@ -17,7 +24,7 @@ const Header: React.FC = () => {
         <styles.Greeting>
           <styles.Nickname>정현</styles.Nickname>님 반가워요!
         </styles.Greeting>
-        <styles.Button>마이페이지</styles.Button>
+        <styles.Button onClick={handleMypage}>마이페이지</styles.Button>
         <styles.Button>로그아웃</styles.Button>
       </styles.RightContainer>
     </styles.Container>
