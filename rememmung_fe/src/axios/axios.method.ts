321fd389674a7f33a8f4e7ee6e8e1cc91975f48d
JJHyeok13 <jjinh99@naver.com>
2024-06-19 21:16:05 +0900

    feat: 편지 생성/목록 조회/상태 수정/삭제 API 구현

--- rememmung_fe/src/axios/axios.method.ts
+++ rememmung_fe/src/axios/axios.method.ts
@@ -3,34 +3,34 @@ import axiosInstance from "@axios/axios.Instance";
 
 export const PostAxiosInstance = async <T>(
   url: string,
-  data?: any
-  // config?: AxiosRequestConfig
+  data?: any,
+  config?: AxiosRequestConfig
 ): Promise<AxiosResponse> => {
-  const response = await axiosInstance.post(url, data);
+  const response = await axiosInstance.post(url, data, config);
   return response;
 };
 
 export const GetAxiosInstance = async <T>(
-  url: string
-  // config?: AxiosRequestConfig
+  url: string,
+  config?: AxiosRequestConfig
 ): Promise<AxiosResponse> => {
-  const res = await axiosInstance.get(url);
+  const res = await axiosInstance.get(url, config);
   return res;
 };
 
 export const PatchAxiosInstance = async <T>(
   url: string,
-  data?: any
-  // config?: AxiosRequestConfig
+  data?: any,
+  config?: AxiosRequestConfig
 ): Promise<AxiosResponse> => {
-  const res = await axiosInstance.patch(url, data);
+  const res = await axiosInstance.patch(url, data, config);
   return res;
 };
 
 export const DeleteAxiosInstance = async <T>(
-  url: string
-  // config?: AxiosRequestConfig
+  url: string,
+  config?: AxiosRequestConfig
 ): Promise<AxiosResponse> => {
-  const res = await axiosInstance.delete(url);
+  const res = await axiosInstance.delete(url, config);
   return res;
 };
---
+++ rememmung_fe/src/server/api/letter.ts
@@ -0,0 +1,85 @@
+import {
+  GetAxiosInstance,
+  PostAxiosInstance,
+  PatchAxiosInstance,
+  DeleteAxiosInstance,
+} from "@axios/axios.method";
+import { GetLetterListConfig } from "@server/config/letter";
+import {
+  UpdateLetterRequest,
+  WriteLetterRequest,
+} from "@server/requestType/letter";
+import {
+  WriteLetterResponse,
+  GetLetterListResponse,
+  UpdateLetterResponse,
+  DeleteLetterResponse,
+} from "@server/responseType/letter";
+
+// 편지 생성하기 /users/me/letters
+export const writeLetter = async (
+  data: WriteLetterRequest
+): Promise<WriteLetterResponse> => {
+  try {
+    const res = await PostAxiosInstance<WriteLetterResponse>(
+      `/users/me/letters`,
+      data
+    );
+
+    return res.data.result;
+  } catch (error) {
+    console.log("편지 생성하기 에러", error);
+    throw error;
+  }
+};
+
+// 편지 목록 조회하기 /users/me/letters
+export const getLetterList = async (
+  config: GetLetterListConfig
+): Promise<GetLetterListResponse> => {
+  try {
+    const res = await GetAxiosInstance<GetLetterListResponse>(
+      `/users/me/letters`,
+      config
+    );
+
+    return res.data.result;
+  } catch (error) {
+    console.log("편지 목록 조회하기 에러", error);
+    throw error;
+  }
+};
+
+// 편지 상태 수정하기 /users/me/letters/{letterId}
+export const updateLetter = async (
+  letterId: number,
+  data: UpdateLetterRequest
+): Promise<UpdateLetterResponse> => {
+  try {
+    const res = await PatchAxiosInstance<UpdateLetterResponse>(
+      `/users/me/letters/${letterId}`,
+      data
+    );
+
+    return res.data.result;
+  } catch (error) {
+    console.log("편지상태 수정하기", error);
+    throw error;
+  }
+};
+
+// 편지 삭제하기 /users/me/letters/{letterId}
+export const deleteLetter = async (
+  letterId: number
+): Promise<DeleteLetterResponse> => {
+  try {
+    const res = await DeleteAxiosInstance<DeleteLetterResponse>(
+      `/users/me/letters/${letterId}`
+    );
+
+    return res.data.result;
+  } catch (error) {
+    console.log("편지 삭제하기 에러", error);
+    throw error;
+  }
+};
