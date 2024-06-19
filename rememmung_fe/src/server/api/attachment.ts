b614cb9e3dbdab2886fc6c373ae28197ceeaf706
JJHyeok13 <jjinh99@naver.com>
2024-06-19 21:37:55 +0900

    feat: 첨부파일 업로드/상태 수정, 펫 첨부파일 업로드 API 구현

---
+++ rememmung_fe/src/server/api/attachment.ts
@@ -0,0 +1,39 @@
+import { PostAxiosInstance } from "@axios/axios.method";
+import { UploadFileRequest } from "@server/requestType/attachment";
+import {
+  UpdateUploadFileResponse,
+  UploadFileResponse,
+} from "@server/responseType/attachment";
+
+// 첨부파일 업로드 /attachment/prepare-upload
+export const uploadFile = async (
+  data: UploadFileRequest
+): Promise<UploadFileResponse> => {
+  try {
+    const res = await PostAxiosInstance<UploadFileResponse>(
+      `/attachment/prepare-upload`,
+      data
+    );
+
+    return res.data.result;
+  } catch (error) {
+    console.log("첨부파일 업로드 에러", error);
+    throw error;
+  }
+};
+
+// 첨부파일 업로드 상태 수정 /attachment/complete-upload/{attachmentId}
+export const updateUploadFile = async (
+  attachmentId: number
+): Promise<UpdateUploadFileResponse> => {
+  try {
+    const res = await PostAxiosInstance<UpdateUploadFileResponse>(
+      `/attachment/complete-upload/${attachmentId}`
+    );
+
+    return res.data.result;
+  } catch (error) {
+    console.log("첨부파일 업로드 상태 수정 에러", error);
+    throw error;
+  }
+};
---
+++ rememmung_fe/src/server/api/pet_attachment.ts
@@ -0,0 +1,19 @@
+import { GetAxiosInstance } from "@axios/axios.method";
+import { GetPetFileResponse } from "@server/responseType/pet_attachment";
+
+// 펫 첨부파일 조회 /users/me/pet-attachments
+export const getPetFile = async (
+  page: number,
+  pageSize: number
+): Promise<GetPetFileResponse> => {
+  try {
+    const res = await GetAxiosInstance<GetPetFileResponse>(
+      `/users/me/pet-attachments?page=${page}&pageSize=${pageSize}`
+    );
+
+    return res.data.result;
+  } catch (error) {
+    console.log("펫 첨부파일 조회 에러", error);
+    throw error;
+  }
+};
