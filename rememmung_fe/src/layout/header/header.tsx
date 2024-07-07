ba6ba167869319cb811305ee1725db18600bd99f
JJHyeok13 <jjinh99@naver.com>
2024-07-07 19:17:45 +0900

    feat: 헤더에 유저정보 조회 호출 버튼 생성

--- rememmung_fe/src/layout/header/header.tsx
+++ rememmung_fe/src/layout/header/header.tsx
@@ -5,10 +5,18 @@ import styles from "./styles";
 
 import LogoImage from "@assets/header/Logo.svg";
 import { logoutUser } from "@server/user/api/oauth";
-import { useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import { userInfo } from "recoil/recoil";
+import { getUserInfo } from "@server/user/api/user";
 
 const Header: React.FC = () => {
+  const [, setUserInfo] = useRecoilState(userInfo);
+
+  const getInfo = () => {
+    console.log("내 정보 조회 호출");
+    getUserInfo().then((res) => setUserInfo(res));
+  };
+
   const userinfo = useRecoilValue(userInfo);
 
   const navigate = useNavigate();
@@ -34,6 +42,7 @@ const Header: React.FC = () => {
         <styles.Greeting>
           <styles.Nickname>{userinfo.userName}</styles.Nickname>님 반가워요!
         </styles.Greeting>
+        <styles.Button onClick={getInfo}>내 정보 조회 API 테스트</styles.Button>
         <styles.Button onClick={handleMypage}>마이페이지</styles.Button>
         <styles.Button onClick={handleLogout}>로그아웃</styles.Button>
       </styles.RightContainer>
