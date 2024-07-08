250632b7f2c511bb36114757b1d4ba1136a80dec
JJHyeok13 <jjinh99@naver.com>
2024-07-08 12:42:23 +0900

    feat: textarea 추가

--- rememmung_fe/src/pages/diaryPage/diaryPage.tsx
+++ rememmung_fe/src/pages/diaryPage/diaryPage.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import RoundImage from "@assets/diaryPage/roundImage.svg";
+
 import styles from "./styles";
 
 const DiaryPage: React.FC = () => {
@@ -7,6 +9,8 @@ const DiaryPage: React.FC = () => {
     <styles.Container>
       <styles.WhiteBox>
         <styles.Date>2024. 07. 07 (일)</styles.Date>
+        <styles.Emotion src={RoundImage} />
+        <styles.TextArea placeholder="오늘 하루에 대해 기록해주세요" />
       </styles.WhiteBox>
     </styles.Container>
   );
--- rememmung_fe/src/pages/diaryPage/styles.tsx
+++ rememmung_fe/src/pages/diaryPage/styles.tsx
@@ -13,6 +13,7 @@ const styles = {
     display: flex;
     flex-direction: column;
     justify-content: center;
+    align-items: center;
     width: 100%;
     height: 580px;
     border-radius: 12px;
@@ -24,16 +25,33 @@ const styles = {
     line-height: 180%;
     letter-spacing: -0.02em;
 
-    padding: 32px 32px 32px 32px;
+    padding: 48px 64px;
     box-sizing: border-box;
 
     margin: 0 auto;
   `,
   Date: styled.div`
     font-family: "YangJin";
-    font-size: 14px;
+    font-size: 18px;
     font-weight: 800;
     color: #37393d;
+
+    margin-bottom: 24px;
+  `,
+  Emotion: styled.img`
+    width: 140px;
+    height: 140px;
+  `,
+  TextArea: styled.textarea`
+    margin-top: 40px;
+    padding: 16px 12px 12px 16px;
+    background-color: #e8e8eb;
+    color: #8a8e99;
+    resize: none;
+    border: 0;
+    border-radius: 12px;
+    width: 100%;
+    height: 194px;
   `,
 };
 
