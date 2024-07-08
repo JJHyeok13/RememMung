20ee56f1b732e0a34364f0ddac8948726cd12d70
JJHyeok13 <jjinh99@naver.com>
2024-07-08 14:52:53 +0900

    fix: 코드 수정

--- rememmung_fe/src/layout/header/header.tsx
+++ rememmung_fe/src/layout/header/header.tsx
@@ -5,19 +5,16 @@ import styles from "./styles";
 
 import LogoImage from "@assets/header/Logo.svg";
 import { logoutUser } from "@server/user/api/oauth";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { userInfo } from "recoil/recoil";
+import { useRecoilState } from "recoil";
+import { userName } from "recoil/recoil";
 import { getUserInfo } from "@server/user/api/user";
 
 const Header: React.FC = () => {
-  const [, setUserInfo] = useRecoilState(userInfo);
+  const [username, setUserName] = useRecoilState(userName);
 
   const getInfo = () => {
-    console.log("내 정보 조회 호출");
-    getUserInfo().then((res) => setUserInfo(res));
+    getUserInfo().then((res) => setUserName(res.data.userName));
   };
-
-  const userinfo = useRecoilValue(userInfo);
 
   const navigate = useNavigate();
 
@@ -40,7 +37,7 @@ const Header: React.FC = () => {
       </styles.StyledLink>
       <styles.RightContainer>
         <styles.Greeting>
-          <styles.Nickname>{userinfo.userName}</styles.Nickname>님 반가워요!
+          <styles.Nickname>{username}</styles.Nickname>님 반가워요!
         </styles.Greeting>
         <styles.Button onClick={getInfo}>내 정보 조회 API 테스트</styles.Button>
         <styles.Button onClick={handleMypage}>마이페이지</styles.Button>
