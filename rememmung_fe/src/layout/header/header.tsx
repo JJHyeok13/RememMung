c1618b4b04cd29834cc5c5b55e09c1b8ab71132a
JJHyeok13 <jjinh99@naver.com>
2024-07-08 15:01:10 +0900

    feat: 헤더 버튼 삭제 및 코드 수정

--- rememmung_fe/src/layout/header/header.tsx
+++ rememmung_fe/src/layout/header/header.tsx
@@ -5,16 +5,11 @@ import styles from "./styles";
 
 import LogoImage from "@assets/header/Logo.svg";
 import { logoutUser } from "@server/user/api/oauth";
-import { useRecoilState } from "recoil";
+import { useRecoilValue } from "recoil";
 import { userName } from "recoil/recoil";
-import { getUserInfo } from "@server/user/api/user";
 
 const Header: React.FC = () => {
-  const [username, setUserName] = useRecoilState(userName);
-
-  const getInfo = () => {
-    getUserInfo().then((res) => setUserName(res.data.userName));
-  };
+  const username = useRecoilValue(userName);
 
   const navigate = useNavigate();
 
@@ -39,7 +34,6 @@ const Header: React.FC = () => {
         <styles.Greeting>
           <styles.Nickname>{username}</styles.Nickname>님 반가워요!
         </styles.Greeting>
-        <styles.Button onClick={getInfo}>내 정보 조회 API 테스트</styles.Button>
         <styles.Button onClick={handleMypage}>마이페이지</styles.Button>
         <styles.Button onClick={handleLogout}>로그아웃</styles.Button>
       </styles.RightContainer>
