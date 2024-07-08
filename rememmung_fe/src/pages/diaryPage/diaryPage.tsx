1e1175a930a7eb7a3ce45ceb6d7f4e869bf0dd57
JJHyeok13 <jjinh99@naver.com>
2024-07-08 12:26:09 +0900

    feat: 멍일기 페이지 기본 배경 추가

---
+++ rememmung_fe/src/pages/diaryPage/diaryPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "./styles";
+
+const DiaryPage: React.FC = () => {
+  return (
+    <styles.Container>
+      <styles.WhiteBox></styles.WhiteBox>
+    </styles.Container>
+  );
+};
+
+export default DiaryPage;
