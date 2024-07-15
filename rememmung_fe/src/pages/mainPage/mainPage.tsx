72b7454538788e3930c144b80a7366e3c0103d16
JJHyeok13 <jjinh99@naver.com>
2024-07-16 03:26:12 +0900

    feat: 메인페이지 styles 삭제 및 tailwind 적용

--- rememmung_fe/src/pages/mainPage/mainPage.tsx
+++ rememmung_fe/src/pages/mainPage/mainPage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 
-import styles from "./styles";
 import { getPetInfo, getUserInfo } from "@server/user/api/user";
+
 import { useRecoilState } from "recoil";
 import { userName, petName } from "recoil/recoil";
 
@@ -15,19 +15,17 @@ const MainPage: React.FC = () => {
   }, []);
 
   return (
-    <styles.Container>
-      <styles.WhiteBox>
-        <div>반가워요!</div>
-        <div>
-          리멤멍은 반려동물과의 이별을 하게된 사람들을 위해 과거의 추억 회상을
-          통해
-        </div>
-        <div>이별을 준비하고 도와주는 것을 목표로 제작된 서비스에요..</div>
-        <div>당신의 반려동물과 건강한 이별을 리멤멍이 도와줄게요!</div>
-        <br />
-        <br />
-      </styles.WhiteBox>
-    </styles.Container>
+    <div className="w-5/6 mx-auto bg-[#f6f6f8] flex flex-col justify-center h-2/3 py-8 pl-8 pr-4 rounded-xl text-[#5d606b] text-lg">
+      <div>반가워요!</div>
+      <div>
+        리멤멍은 반려동물과의 이별을 하게된 사람들을 위해 과거의 추억 회상을
+        통해
+      </div>
+      <div>이별을 준비하고 도와주는 것을 목표로 제작된 서비스에요..</div>
+      <div>당신의 반려동물과 건강한 이별을 리멤멍이 도와줄게요!</div>
+      <br />
+      <br />
+    </div>
   );
 };
 
