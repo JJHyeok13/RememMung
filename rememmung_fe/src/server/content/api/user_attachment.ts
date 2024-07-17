16f56bace728fb91d67f4400e8ce86dc42e3e797
JJHyeok13 <jjinh99@naver.com>
2024-07-17 10:50:03 +0900

    feat: 유저 관련 사진 저장 API 구현

--- rememmung_fe/src/server/content/api/user_attachment.ts
+++ rememmung_fe/src/server/content/api/user_attachment.ts
@@ -1,5 +1,9 @@
-import { GetAxiosInstance } from "@axios/content/axios.method";
+import {
+  GetAxiosInstance,
+  PostAxiosInstance,
+} from "@axios/content/axios.method";
 import { GetUserFileResponse } from "../responseType/user_attachment";
+import { SaveFileRequest } from "../requestType/user_attachment";
 
 // 유저 첨부파일 리스트 조회 /users/me/user-attachments
 export const getPetFile = async (
@@ -15,6 +19,19 @@ export const getPetFile = async (
     return res.data;
   } catch (error) {
     console.log("유저 첨부파일 조회 에러", error);
+    throw error;
+  }
+};
+
+// 유저 관련 사진 저장 /users/me/user-attachments
+export const saveUserFile = async (data: SaveFileRequest) => {
+  try {
+    console.log("유저 관련 사진 저장 API 호출");
+    const res = await PostAxiosInstance(`/users/me/user-attachments`, data);
+
+    return res.data;
+  } catch (error) {
+    console.log("유저 관련 사진 저장 에러", error);
     throw error;
   }
 };
---
+++ rememmung_fe/src/server/content/requestType/user_attachment.ts
@@ -0,0 +1,5 @@
+// 유저 관련 사진 저장 Request Body
+export interface SaveFileRequest {
+  attachmentId: number;
+  isDefault: boolean;
+}
