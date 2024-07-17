7e03737ad296b5e8a999c151d0340a15acb1ff8b
JJHyeok13 <jjinh99@naver.com>
2024-07-17 10:52:13 +0900

    feat: 유저 관련 사진 수정 API 구현

--- rememmung_fe/src/server/content/api/user_attachment.ts
+++ rememmung_fe/src/server/content/api/user_attachment.ts
@@ -3,7 +3,10 @@ import {
   PostAxiosInstance,
 } from "@axios/content/axios.method";
 import { GetUserFileResponse } from "../responseType/user_attachment";
-import { SaveFileRequest } from "../requestType/user_attachment";
+import {
+  SaveFileRequest,
+  UpdateFileRequest,
+} from "../requestType/user_attachment";
 
 // 유저 첨부파일 리스트 조회 /users/me/user-attachments
 export const getPetFile = async (
@@ -32,6 +35,25 @@ export const saveUserFile = async (data: SaveFileRequest) => {
     return res.data;
   } catch (error) {
     console.log("유저 관련 사진 저장 에러", error);
+    throw error;
+  }
+};
+
+// 유저 관련 사진 수정 /users/me/user-attachments/{userAttachmentId}
+export const updateUserFile = async (
+  userAttachmentId: number,
+  data: UpdateFileRequest
+) => {
+  try {
+    console.log("유저 관련 사진 수정 API 호출");
+    const res = await PostAxiosInstance(
+      `/users/me/user-attachments/${userAttachmentId}`,
+      data
+    );
+
+    return res.data;
+  } catch (error) {
+    console.log("유저 관련 사진 수정 에러", error);
     throw error;
   }
 };
--- rememmung_fe/src/server/content/requestType/user_attachment.ts
+++ rememmung_fe/src/server/content/requestType/user_attachment.ts
@@ -3,3 +3,9 @@ export interface SaveFileRequest {
   attachmentId: number;
   isDefault: boolean;
 }
+
+// 유저 관련 사진 수정 Request Body
+export interface UpdateFileRequest {
+  attachmentId: number;
+  isDefault: boolean;
+}
